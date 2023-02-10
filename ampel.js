419e9568f3a02113067028cfe1e1694a2d4c480b
iurimach <119754002+iurimach@users.noreply.github.com>
2023-02-10 21:52:35 +0400

    update

---
+++ ampel.js
@@ -0,0 +1,73 @@
+function myFunction() {
+    var witeli= document.querySelector('.witeli'); //witeli
+    var mwvane= document.querySelector('.mwvane');
+    var yviteli= document.querySelector('.yviteli');
+
+    mwvane.style.animationName="non"
+    witeli.style.animationName="non"
+    yviteli.style.animationName="non"
+
+    mwvane.style.backgroundColor="white"
+    witeli.style.backgroundColor="red"
+    yviteli.style.backgroundColor="white"
+
+   
+  }
+
+  function myFunction2() {
+    var mwvane= document.querySelector('.mwvane');   //mwvane
+    var witeli= document.querySelector('.witeli');
+    var yviteli= document.querySelector('.yviteli');
+
+    mwvane.style.animationName="non"
+    witeli.style.animationName="non"
+    yviteli.style.animationName="non"
+
+    mwvane.style.backgroundColor="green"
+    witeli.style.backgroundColor="white"
+    yviteli.style.backgroundColor="white"
+    
+
+   
+  }
+
+  function myFunction3() {
+    var yviteli= document.querySelector('.yviteli'); //yviteli
+    var mwvane= document.querySelector('.mwvane');
+    var witeli= document.querySelector('.witeli');
+
+    mwvane.style.animationName="non"
+    witeli.style.animationName="non"
+    yviteli.style.animationName="non"
+
+    mwvane.style.backgroundColor="white"
+    witeli.style.backgroundColor="white"
+    yviteli.style.backgroundColor="yellow"
+    
+
+   
+  }
+
+  function myFunction4() {
+    var mwvane= document.querySelector('.mwvane');   //mwvane
+    var witeli= document.querySelector('.witeli');
+    var yviteli= document.querySelector('.yviteli');
+
+    mwvane.style.animationName="non"
+    witeli.style.animationName="non"
+    yviteli.style.animationName="non"
+
+    mwvane.style.backgroundColor="white"
+    witeli.style.backgroundColor="white"
+    yviteli.style.backgroundColor="white"
+  }
+
+  function myFunction5() {
+    var mwvane= document.querySelector('.mwvane');   //mwvane
+    var witeli= document.querySelector('.witeli');
+    var yviteli= document.querySelector('.yviteli');
+
+    mwvane.style.animationName="mwvane1"
+    witeli.style.animationName="witeli"
+    yviteli.style.animationName="yviteli1"
+  }
